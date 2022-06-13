691651d5be96afb408aaf23ff110ec6d6f131889
technoalli3 <92867135+technoalli3@users.noreply.github.com>
2022-06-13 09:54:00 -0400

    updated config

--- src/main/java/com/alli/gamebreaker/mixin/WeightedSoundsMixin.java
+++ src/main/java/com/alli/gamebreaker/mixin/WeightedSoundsMixin.java
@@ -7,10 +7,14 @@ import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
+import static com.alli.gamebreaker.GameBreakerMain.CONFIG;
+
 @Mixin(WeightedSoundSet.class)
 public class WeightedSoundsMixin {
     @Inject(method="getSubtitle", at=@At("HEAD"), cancellable = true)
     private void changeSubtitle(CallbackInfoReturnable<Text> cir) {
-        cir.setReturnValue(Text.of("[insert subtitle here]"));
+        if(CONFIG.overrideSubtitles) {
+            cir.setReturnValue(Text.of("[insert subtitle here]"));
+        }
     }
 }
