ef59e39eeb31e7fdee604dd0c0e0293b5b7a35ad
technoalli3 <92867135+technoalli3@users.noreply.github.com>
2022-06-13 10:33:53 -0400

    Update DoublePerlinMixin.java

--- src/main/java/com/alli/gamebreaker/mixin/DoublePerlinMixin.java
+++ src/main/java/com/alli/gamebreaker/mixin/DoublePerlinMixin.java
@@ -20,39 +20,10 @@ public class DoublePerlinMixin {
     @Inject(method="sample", at=@At("TAIL"), cancellable = true)
     private void changeSample(double x, double y, double z, CallbackInfoReturnable<Double> cir) {
 
-
-
-        /*
-        double sinX = Math.sin(Math.sqrt(Math.abs(z*x)));
-        double sinY = Math.sin(Math.sqrt(Math.abs(x*y)));
-        double sinZ = Math.sin(Math.sqrt(Math.abs(z*y)));
-        cir.setReturnValue(Math.cos(sinX + sinY + sinZ));
-        */
-
-        /**fave so far **/
-
         double sinX = Math.sin(Math.sqrt(Math.abs(x)));
         double sinY = Math.sin(Math.sqrt(Math.abs(y)));
         double sinZ = Math.sin(Math.sqrt(Math.abs(z)));
         cir.setReturnValue(Math.cos(sinX + sinY + sinZ));
-
-
-        /*
-        double sinX = Math.sin(Math.pow(x,2));
-        double sinY = Math.sin(Math.pow(y,2));
-        double sinZ = Math.sin(Math.pow(z,2));
-        cir.setReturnValue(Math.cos(sinX + sinY + sinZ));
-        */
-
-
-
-        /*
-        double sinX = Math.sin(Math.sqrt(x));
-        double sinY = Math.sin(Math.sqrt(y));
-        double sinZ = Math.sin(Math.sqrt(z));
-        cir.setReturnValue(Math.cos(sinX + sinY + sinZ));
-        */
-
-        //alternative cos(x+y+z);
+        
     }
 }
