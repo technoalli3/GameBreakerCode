4869d0bdd525b1545d50467556c3835c299f1367
technoalli3 <92867135+technoalli3@users.noreply.github.com>
2022-06-13 19:46:03 -0400

    added plant config

--- src/main/java/com/alli/gamebreaker/mixin/PlantBlockMixin.java
+++ src/main/java/com/alli/gamebreaker/mixin/PlantBlockMixin.java
@@ -6,15 +6,18 @@ import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.BlockView;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Overwrite;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+import static com.alli.gamebreaker.GameBreakerMain.CONFIG;
 
 @Mixin(PlantBlock.class)
 public class PlantBlockMixin {
-    /**
-     * @author TechnoAlli3
-     * @reason chaos
-     */
-    @Overwrite
-    public boolean canPlantOnTop(BlockState floor, BlockView world, BlockPos pos) {
-        return true;
+    @Inject(method="canPlantOnTop", at=@At("HEAD"), cancellable = true)
+    private void changePlantCheck(BlockState floor, BlockView world, BlockPos pos, CallbackInfoReturnable<Boolean> cir) {
+        if(CONFIG.plantAnywhere) {
+            cir.setReturnValue(true);
+        }
     }
 }
