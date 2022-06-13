92f9d3a2ae9b37a05a6db8d1a6e87bea8285367a
technoalli3 <92867135+technoalli3@users.noreply.github.com>
2022-06-13 09:48:09 -0400

    optimized imports

--- src/main/java/com/alli/gamebreaker/mixin/MathHelperMixin.java
+++ src/main/java/com/alli/gamebreaker/mixin/MathHelperMixin.java
@@ -2,11 +2,7 @@ package com.alli.gamebreaker.mixin;
 
 import net.minecraft.util.Util;
 import net.minecraft.util.math.MathHelper;
-import org.checkerframework.checker.units.qual.A;
-import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Overwrite;
-import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
@@ -15,7 +11,6 @@ import java.util.Random;
 
 import static com.alli.gamebreaker.GameBreakerMain.CONFIG;
 import static net.minecraft.util.math.MathHelper.clamp;
-import static net.minecraft.util.math.MathHelper.subtractAngles;
 
 @Mixin(MathHelper.class)
 public class MathHelperMixin {
