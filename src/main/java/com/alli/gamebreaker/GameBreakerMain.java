ea656506b11723a238de57c6c28494a3ffe6a495
technoalli3 <92867135+technoalli3@users.noreply.github.com>
2022-06-13 21:27:55 -0400

    stuff

--- src/main/java/com/alli/gamebreaker/GameBreakerMain.java
+++ src/main/java/com/alli/gamebreaker/GameBreakerMain.java
@@ -6,7 +6,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class GameBreakerMain implements ModInitializer {
-	// This logger is used to write text to the console and the log file.=
+	public static final String MOD_ID = "gamebreaker";
 
 	@Override
 	public void onInitialize(ModContainer mod) {
---
+++ src/main/java/com/alli/gamebreaker/mixin/SoundMixin.java
@@ -0,0 +1,28 @@
+package com.alli.gamebreaker.mixin;
+
+
+import net.minecraft.client.sound.Sound;
+import net.minecraft.util.Identifier;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+import static com.alli.gamebreaker.GameBreakerMain.MOD_ID;
+
+@Mixin(Sound.class)
+public class SoundMixin {
+	@Inject(method="getLocation", at=@At("HEAD"), cancellable = true)
+	private void changeSound(CallbackInfoReturnable<Identifier> cir) {
+		if(CONFIG.overrideSounds) {
+			int soundIndex = CONFIG.chosenSound;
+			if(soundIndex == 1) {
+				cir.setReturnValue(new Identifier(MOD_ID, "sounds/ping.ogg"));
+			} else if(soundIndex == 2) {
+				cir.setReturnValue(new Identifier(MOD_ID, "sounds/oof.ogg"));
+			} else if(soundIndex == 3) {
+				cir.setReturnValue(new Identifier(MOD_ID, "sounds/heh.ogg"));
+			}
+		}
+	}
+}
