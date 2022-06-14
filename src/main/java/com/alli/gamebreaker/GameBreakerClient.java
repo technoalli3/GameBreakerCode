7fa479f6bf4383835348e51d862becdadcdd1daa
technoalli3 <92867135+technoalli3@users.noreply.github.com>
2022-06-13 21:02:47 -0400

    quilt transfer

---
+++ src/main/java/com/alli/gamebreaker/GameBreakerClient.java
@@ -0,0 +1,11 @@
+package com.alli.gamebreaker;
+
+import org.quiltmc.loader.api.ModContainer;
+import org.quiltmc.qsl.base.api.entrypoint.client.ClientModInitializer;
+
+public class GameBreakerClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient(ModContainer mod) {
+
+	}
+}
---
+++ src/main/java/com/alli/gamebreaker/GameBreakerMain.java
@@ -0,0 +1,15 @@
+package com.alli.gamebreaker;
+
+import org.quiltmc.loader.api.ModContainer;
+import org.quiltmc.qsl.base.api.entrypoint.ModInitializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class GameBreakerMain implements ModInitializer {
+	// This logger is used to write text to the console and the log file.=
+
+	@Override
+	public void onInitialize(ModContainer mod) {
+
+	}
+}
