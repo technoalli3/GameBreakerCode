ab09cad8d448ec086f988137b5217372b9a0bd84
technoalli3 <92867135+technoalli3@users.noreply.github.com>
2022-06-13 21:58:09 -0400

    more stuff

---
+++ src/main/java/com/alli/gamebreaker/mixin/DoublePerlinMixin.java
@@ -0,0 +1,31 @@
+package com.alli.gamebreaker.mixin;
+
+import net.minecraft.util.math.noise.DoublePerlinNoiseSampler;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+import java.util.Random;
+
+@Mixin(DoublePerlinNoiseSampler.class)
+public class DoublePerlinMixin {
+	private static final Random rand = new Random();
+
+	@Inject(method="createAmplitude", at=@At("TAIL"), cancellable = true)
+	private static void changeAmplitude(int octaves, CallbackInfoReturnable<Double> cir) {
+		//wrap in isEnabled config
+			cir.setReturnValue(Math.sin(octaves));
+	}
+
+	@Inject(method="sample", at=@At("TAIL"), cancellable = true)
+	private void changeSample(double x, double y, double z, CallbackInfoReturnable<Double> cir) {
+
+		//wrap in isEnabled config
+			double sinX = Math.sin(Math.sqrt(Math.abs(x)));
+			double sinY = Math.sin(Math.sqrt(Math.abs(y)));
+			double sinZ = Math.sin(Math.sqrt(Math.abs(z)));
+			cir.setReturnValue(Math.cos(sinX + sinY + sinZ));
+
+	}
+}
