974a99c3f03026886d872aa57b40b535f5d3045f
technoalli3 <92867135+technoalli3@users.noreply.github.com>
2022-06-19 17:26:14 -0400

    optimized imports

--- src/main/java/com/alli/gamebreaker/mixin/PlantBlockMixin.java
+++ src/main/java/com/alli/gamebreaker/mixin/PlantBlockMixin.java
@@ -5,7 +5,6 @@ import net.minecraft.block.PlantBlock;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.BlockView;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Overwrite;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
